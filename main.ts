142f02662dd593166322582cfb1fecf7fdaa9f20
Hlusinka <hlusinp@gmail.com>
2024-12-16 10:15:25 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+let stopky = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+    `)
+})
+
+basic.pause(randint(2000, 8000))
+basic.clearScreen()
+music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 
